22e6c96dce2dd796cb38bb82aadd74050bb8ac58
Sviatoslav Saraev <boyslavic@gmail.com>
2025-04-30 17:21:12 +0200

    Created an abstract class of loaders and pdf loaders, text chunker as well

---
+++ app/processing/text_chunk.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Optional
+from datetime import datetime
+import hashlib
+
+@dataclass
+class TextChunk:
+    text: str
+    source: Path                        # Path to the file
+    page: Optional[int] = None          # Page in PDF
+    chunk_id: Optional[str] = None      # ID unique of chunk
+
+    # Metaadata
+    file_name: str                      # File name
+    last_modified: Optional[datetime] = None    # Time of last change
+    file_type: Optional[str] = None             # 'pdf', 'json', etr.
+    text_hash: str = field(init=False)
+
+    def __post_init__(self):
+        self.text_hash = hashlib.sha256(self.text.encode()).hexdigest()
