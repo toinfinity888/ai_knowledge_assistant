2b3d2f4a7e8844a775f98649d6f38b8e502a6f37
Sviatoslav Saraev <boyslavic@gmail.com>
2025-05-22 13:03:10 +0200

    Gunicorn try1

--- main.py
+++ main.py
@@ -111,3 +111,10 @@ if __name__ == "__main__":
         port=port,
         debug=True
     )
+
+from app import create_app
+
+app = create_app()
+
+#if __name__ == "__main__":
+ #   app.run(debug=True)
